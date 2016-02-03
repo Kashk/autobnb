ca217ac23649605eef6a9b28b887c15f41adf36a
Zain Memon <zain@inzain.net>
2016-02-02 18:59:48 -0800

    List residents in tonight mgmt command

--- guests/management/commands/tonight.py
+++ guests/management/commands/tonight.py
@@ -3,7 +3,7 @@ import datetime
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
-from guests.models import Reservation
+from guests.models import Reservation, Resident
 
 
 class Command(BaseCommand):
@@ -12,4 +12,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         resos = Reservation.objects.filter(dates__contains=datetime.date.today())
         for reso in resos:
-            self.stdout.write("%s: %s" % (reso.confirmation_code, reso.name))
+            if reso.thread_id:
+                self.stdout.write("%s: %s" % (reso.confirmation_code, reso.name))
+            else:
+                self.stdout.write("%s: %s (no thread_id!!!)" % (reso.confirmation_code, reso.name))
+
+        self.stdout.write("----- now residents! ------")
+
+        residents = Resident.objects.filter(is_active=True)
+        for resident in residents:
+            if resident.thread_id:
+                self.stdout.write("%s: %s" % (resident.slug, resident.label))
+            else:
+                self.stdout.write("%s: %s (no thread_id!!!)" % (resident.slug, resident.label))
