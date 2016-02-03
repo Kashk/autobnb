f25dc406c09bf4692175853cc90b64912b0e5ea8
Zain Memon <zain@inzain.net>
2016-02-02 19:21:02 -0800

    Debug support for notifications

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,13 @@
 import datetime
 
+from django.conf import settings
 from django.http import HttpResponseNotFound, HttpResponseGone, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils import timezone
 
 from chat.models import Message
 from guests.models import Reservation, Resident
+from syncer.sync import AirbnbAPI
 
 
 def render_chat_page(request):
@@ -20,6 +22,32 @@ def render_chat_page(request):
         'all_resos': all_resos,
         'all_residents': all_residents,
     })
+
+
+def new_message_notification(posted_by, text):
+    resos = Reservation.objects.filter(dates__contains=datetime.date.today()
+        ).exclude(thread_id=None).exclude(thread_id="").values_list('thread_id', flat=True)
+    residents = Resident.objects.filter(is_active=True
+        ).exclude(thread_id=None).exclude(thread_id="").values_list('thread_id', flat=True)
+
+    thread_ids = list(resos) + list(residents)
+
+    thread_ids = [152531217]  # hardcoded to Duncan
+    resos = Reservation.objects.filter(dates__contains=datetime.date.today()
+        ).exclude(thread_id=None).exclude(thread_id="")
+    residents = Resident.objects.filter(is_active=True
+        ).exclude(thread_id=None).exclude(thread_id="")
+    names = [reso.name for reso in resos] + [resident.label for resident in residents]
+
+    message = """
+Hi! This message was sent to the whole house.
+%s says: %s
+DEBUG INFO: This notification would've been sent to: %s
+""" % (posted_by, text, names)
+
+    airbnb = AirbnbAPI(settings.AIRBNB_USERNAME, settings.AIRBNB_PASSWORD)
+    for thread_id in thread_ids:
+        airbnb.send_message(thread_id, message)
 
 
 def reso(request, confirmation_code):
@@ -39,6 +67,7 @@ def reso(request, confirmation_code):
         posted_by = "%s from %s" % (reso.name, reso.location)
         picture = reso.picture
         Message.objects.create(text=request.POST['msg'], posted_by=posted_by, picture=picture)
+        new_message_notification(posted_by, request.POST['msg'])
 
     return render_chat_page(request)
 
@@ -56,5 +85,6 @@ def resident(request, slug):
         posted_by = resident.label
         picture = resident.picture
         Message.objects.create(text=request.POST['msg'], posted_by=posted_by, picture=picture)
+        new_message_notification(posted_by, request.POST['msg'])
 
     return render_chat_page(request)
